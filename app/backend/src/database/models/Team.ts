c707a203be1f131208253c9c0edb52ec3358708e
Rafael <rafinhabzf@gmail.com>
2023-03-29 17:01:30 -0300

    testando a configuração do projeto

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,26 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    type: STRING,
+  },
+}, {
+  modelName: 'teams',
+  sequelize: db,
+  timestamps: false,
+  underscored: true,
+});
+
+export default Team;
