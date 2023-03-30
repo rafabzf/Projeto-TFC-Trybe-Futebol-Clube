076fb77aa23ba2ca330d7d4ce980455e99a241c6
Rafael <rafinhabzf@gmail.com>
2023-03-30 19:29:46 -0300

    Testando requisito 2

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,24 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp from 'chai-http';
+import { app } from '../app';
+import Team from '../database/models/Team';
+import { all } from './mocks/mockTeams';
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Testando model de Teams', () => {
+  it('Testa se retorna todos os times', async () => {
+    sinon.stub(Team, 'findAll')
+    .resolves(all);
+
+    const res: Response = await chai
+    .request(app)
+    .get('/teams');
+
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.deep.equal(all);
+  });
+});
